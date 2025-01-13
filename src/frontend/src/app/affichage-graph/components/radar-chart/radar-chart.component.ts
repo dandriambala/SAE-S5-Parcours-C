e8476d89d1ff0ba79f208ff00c8d1b4c8058ad5f
Immannn <i.amimman0.0@gmail.com>
2025-01-13 13:52:24 +0100

    radar-chart

--- src/frontend/src/app/affichage-graph/components/radar-chart/radar-chart.component.ts
+++ src/frontend/src/app/affichage-graph/components/radar-chart/radar-chart.component.ts
@@ -18,8 +18,8 @@ export class RadarChartComponent implements OnInit {
   yAxis: boolean = true;
   showXAxisLabel: boolean = true;
   showYAxisLabel: boolean = true;
-  xAxisLabel: string = 'Élément';
-  yAxisLabel: string = 'Valeur';
+  xAxisLabel: string = '';
+  yAxisLabel: string = '';
   legend: boolean = true;
   showLabels: boolean = true;
   animations: boolean = true;
@@ -32,18 +32,67 @@ export class RadarChartComponent implements OnInit {
 
   loadChartData(): void {
     this.studentService.getStudentData().subscribe((students) => {
-      this.multi = students.slice(0, 5).map((student, index) => ({
-        name: `Étudiant ${index + 1}`,
-        series: [
-          { name: 'Moyenne G1-G3', value: (student.G1 + student.G2 + student.G3) / 3 },
-          { name: 'Absences', value: student.absences },
-          { name: 'Temps libre', value: student.freetime },
-          { name: 'Temps d\'étude', value: student.studytime },
-          { name: 'Échecs', value: student.failures },
-        ],
-      }));
+    
+      const noteRanges = [
+        { name: '< 5', series: this.initializeCriteria() },
+        { name: '5-10', series: this.initializeCriteria() },
+        { name: '10-15', series: this.initializeCriteria() },
+        { name: '> 15', series: this.initializeCriteria() },
+      ];
+  
+     
+      students.forEach((student) => {
+        const moyenne = (student.G1 + student.G2 + student.G3) / 3;
+  
+        
+        let rangeIndex = -1;
+        if (moyenne < 5) {
+          rangeIndex = 0;
+        } else if (moyenne >= 5 && moyenne < 10) {
+          rangeIndex = 1;
+        } else if (moyenne >= 10 && moyenne < 15) {
+          rangeIndex = 2;
+        } else {
+          rangeIndex = 3;
+        }
+  
+       
+        if (rangeIndex !== -1) {
+          const range = noteRanges[rangeIndex];
+          range.series[0].value += student.absences; 
+          range.series[1].value += student.freetime; 
+          range.series[2].value += student.studytime; 
+          range.series[3].value += student.failures; 
+          range.series[4].value++; 
+        }
+      });
+  
+     
+      noteRanges.forEach((range) => {
+        const studentCount = range.series.find(s => s.name === 'Nombre d\'étudiants')?.value || 1;
+        range.series.forEach((criterion) => {
+          if (criterion.name !== 'Nombre d\'étudiants') {
+            criterion.value = criterion.value / studentCount;
+          }
+        });
+      });
+  
+     
+      this.multi = noteRanges;
     });
   }
+  
+ 
+  private initializeCriteria() {
+    return [
+      { name: 'Absences', value: 0 },
+      { name: 'Temps libre', value: 0 },
+      { name: 'Temps d\'étude', value: 0 },
+      { name: 'Échecs', value: 0 },
+      { name: 'Nombre d\'étudiants', value: 0 }, 
+    ];
+  }
+  
 
   onSelect(event: any): void {
     console.log(event);
