52782353f8bd1a80922da4fab2325a33c748c410
emarielouise972 <marielouiseesteban90@gmail.com>
2025-01-20 11:53:14 +0100

    boutons en savoir plus

--- src/frontend/src/app/kaggle-description/kaggle-description.component.ts
+++ src/frontend/src/app/kaggle-description/kaggle-description.component.ts
@@ -1,12 +1,18 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-kaggle-description',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './kaggle-description.component.html',
   styleUrl: './kaggle-description.component.css'
 })
 export class KaggleDescriptionComponent {
+  showFullText: boolean = false;
+
+  toggleText() {
+    this.showFullText = !this.showFullText;
+  }
 
 }
