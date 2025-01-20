ac5f46f75486529a906dba13a4e7ad494daaccb1
IMMAN <i.amimman0.0@gmai.com>
2025-01-20 15:09:31 +0100

    develop + correction beug

--- src/backend/models/reasons_model.py
+++ src/backend/models/reasons_model.py
@@ -1,9 +1,6 @@
 from bd_sql import db
 
 class Reason(db.Model):
-    __tablename__ = 'reasons'
-    reason_id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)  # Clé primaire
-    reason_name = db.Column(db.String(50))  # Nom de la raison
-
-    def __repr__(self):
-        return f"<Reasons reason_id={self.reason_id} reason_name={self.reason_name}>"
+    __tablename__ = 'reason'
+    reason_id = db.Column(db.BigInteger, primary_key=True)
+    reason = db.Column(db.String(50), nullable=True)
